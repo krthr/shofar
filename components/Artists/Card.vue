a04525a145f76effeff288f79ac5eaa8fc362217
Wilson <wtovaragudelo@gmail.com>
2024-01-03 09:58:33 +0100

    feat: artists card

---
+++ components/Artists/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card card-compact hover:bg-base-25 group">
+    <div class="card-body text-center">
+      <div class="w-full relative">
+        <button
+          class="absolute btn-circle btn btn-primary bottom-2 right-2 hidden group-hover:block shadow-lg"
+        >
+          <Icon name="ph:play-fill" />
+        </button>
+
+        <NuxtLink :to="{ name: 'artists-id', params: { id: artist.id } }">
+          <img :src="artist.thumbnail!" class="rounded-full" />
+        </NuxtLink>
+      </div>
+
+      <NuxtLink :to="{ name: 'artists-id', params: { id: artist.id } }">
+        <h2
+          class="text-center card-title line-clamp-1 text-sm md:text-base font-bold"
+        >
+          {{ artist.name }}
+        </h2>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { GetArtists } from "~/server/api/artists";
+import type { Search } from "~/server/api/search";
+
+defineProps<{
+  artist: GetArtists["data"][number] | Search["artists"][number];
+}>();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-5 page-width">
+    <!-- <h1 class="text-xl font-medium">Artistas</h1> -->
+
+    <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
+      <ArtistsCard
+        v-for="artist in artists.data"
+        :key="artist.id"
+        :artist="artist"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/artists");
+const artists = data.value!;
+</script>
