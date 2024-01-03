e154bd9f23a9f12bef65f7f1eaffb92ba3e7cbdd
Wilson <wtovaragudelo@gmail.com>
2024-01-03 10:15:36 +0100

    feat: app

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full pb-[150px]">
+    <Navbar />
+
+    <div>
+      <NuxtPage class="pt-[90px]" />
+    </div>
+
+    <!-- <LazyPlayer /> -->
   </div>
 </template>
