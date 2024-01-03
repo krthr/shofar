ba58ab1ad89d2be75e0ef494873f149051762f40
Wilson <wtovaragudelo@gmail.com>
2024-01-03 10:51:23 +0100

    feat: albums components

---
+++ components/Albums/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex items-end gap-4 flex-wrap justify-center page-width">
+    <div class="flex">
+      <img
+        :src="album.thumbnail!"
+        class="rounded-md w-[200px] h-[200px] aspect-square"
+      />
+    </div>
+
+    <div class="flex-initial md:flex-1 text-center md:text-left">
+      <h1 class="font-medium text-4xl md:text-5xl line-clamp-1">
+        {{ album.name }}
+      </h1>
+
+      <div>
+        <NuxtLink
+          v-for="{ artist } in album.artists"
+          :key="artist.id"
+          class="hover:underline font-medium"
+          :to="{ name: 'artists-id', params: { id: artist.id } }"
+        >
+          {{ artist.name }}
+        </NuxtLink>
+      </div>
+
+      <PlayGroupButton />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { GetAlbum } from "~/server/api/albums/[id]";
+
+defineProps<{ album: NonNullable<GetAlbum> }>();
+</script>
---
+++ components/Albums/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card card-compact card-bordered hover:bg-base-25 group">
+    <div class="card-body">
+      <div class="w-full relative">
+        <button
+          class="absolute btn-circle btn btn-primary bottom-2 right-2 hidden group-hover:block shadow-lg"
+        >
+          <Icon name="ph:play-fill" />
+        </button>
+
+        <NuxtLink :to="{ name: 'albums-id', params: { id: album.id } }">
+          <img :src="album.thumbnail!" class="rounded-md hover:shadow-2xl" />
+        </NuxtLink>
+      </div>
+
+      <div class="line-clamp-1 font-bold card-title text-sm">
+        <NuxtLink :to="{ name: 'albums-id', params: { id: album.id } }">
+          {{ album.name }}
+        </NuxtLink>
+      </div>
+
+      <!-- <div v-if="showArtist" class="font-light text-xs">
+        <NuxtLink
+          class="hover:underline"
+          :to="{ name: 'artists-id', params: { id: album.artists.at(0)?.id } }"
+        >
+          {{ album.artists.at(0)?.name }}
+        </NuxtLink>
+      </div> -->
+
+      <!-- <div class="font-light text-xs">2022</div> -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { GetArtist } from "~/server/api/artists/[id]";
+
+defineProps<{
+  album: NonNullable<GetArtist>["albums"][number]["album"];
+  showArtist?: boolean;
+}>();
+</script>
